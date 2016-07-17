2d565c8b38b20909ceb02d993be0994097add8fe
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-07-16 18:56:19 -0600

    Add first tests for grid cells aka shots

--- lib/grid_cell.rb
+++ lib/grid_cell.rb
@@ -1,3 +1,9 @@
 class GridCell
-
+  attr_reader :status,
+              :ships
+              
+  def initialize(status = :open, ships = nil)
+    @status = status
+    @ships = ships
+  end
 end
--- test/gridcell_test.rb
+++ test/gridcell_test.rb
@@ -3,6 +3,14 @@ require 'minitest/pride'
 require './lib/grid_cell'
 
 class GridCellTest < Minitest::Test
+  def test_it_starts_with_open_cells
+    grid = GridCell.new
+    assert_equal :open, grid.status
+  end
 
+  def test_it_starts_with_no_ships
+    grid = GridCell.new
+    assert_equal nil, grid.ships
+  end
 
 end
