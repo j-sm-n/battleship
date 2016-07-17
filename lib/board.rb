4c96d156fb99c0a737d6b5bc6cf07d65ead674f7
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-07-17 15:57:30 -0600

    can generate board with grid cell instances inside each cell

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,14 @@
+require './grid_cell'
+
 class Board
+  attr_reader :grid
 
   NUM_SHIPS = 5
+  BOARD_DIM = 5
   ROW = ['A','B','C','D']
   COLUMN = ['1','2','3','4']
 
+  def initialize
+    @grid = Array.new(BOARD_DIM).map! {Array.new(BOARD_DIM).map! {GridCell.new}}
+  end
 end
