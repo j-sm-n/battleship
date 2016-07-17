eca38dd9450a59596d77856ef2710c77db0af655
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-07-17 17:00:00 -0600

    Added Gemfile and SimpleCov to board_test

--- lib/board.rb
+++ lib/board.rb
@@ -3,10 +3,10 @@ require './lib/grid_cell'
 class Board
   attr_reader :grid
 
-  NUM_SHIPS = 5
+  # NUM_SHIPS = 2
   BOARD_DIM = 5
-  ROW = ['A','B','C','D']
-  COLUMN = ['1','2','3','4']
+  # ROW = ['A','B','C','D']
+  # COLUMN = ['1','2','3','4']
 
   def initialize
     @grid = Array.new(BOARD_DIM).map! {Array.new(BOARD_DIM).map! {GridCell.new}}
