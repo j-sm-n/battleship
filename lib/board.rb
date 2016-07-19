c6afee635a86bc7a01fc83680c26761f07ab0c2b
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-07-19 13:15:28 -0600

    Changed board_dim to 4 from 5

--- lib/board.rb
+++ lib/board.rb
@@ -6,9 +6,9 @@ class Board
   attr_reader :grid
 
   NUM_SHIPS = 2
-  BOARD_DIM = 5
-  ROW       = ['A','B','C','D','E']
-  COLUMN    = ['1','2','3','4','5']
+  BOARD_DIM = 4
+  ROW       = ['A','B','C','D']
+  COLUMN    = ['1','2','3','4']
 
   def initialize
     @grid = Array.new(BOARD_DIM).map! {Array.new(BOARD_DIM).map! {GridCell.new}}
@@ -17,18 +17,21 @@ class Board
   def dispaly_board
     row_letter = ('A'..'Z').to_a
     counter = 0
-    puts "==========="
-    puts ". 1 2 3 4 5"
+    puts "=========="
+    puts ". 1 2 3 4"
     @grid.each do |row|
       print row_letter[counter] + ' '
       row.each {|cell| print cell.to_s + ' '}
       print "\n"
       counter += 1
     end
-    puts "==========="
+    puts "=========="
   end
 
   def valid_coordinates?(ship, first_cord, second_cord)
     true
   end
 end
+
+board = Board.new
+board.dispaly_board
